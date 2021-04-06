97cec721e111ca393f3380bf82fe70b9d000ef1b
xiao <779740337@qq.com>
2021-04-06 19:04:04 +0800

    改善了一下简历的样式

--- src/component/ProjectExperience/ProjectExperience.jsx
+++ src/component/ProjectExperience/ProjectExperience.jsx
@@ -4,18 +4,20 @@ import "./index.scss"
 export default function ProjectExperience(props) {
     return (
         <div className={"ProjectExperience"}>
-            <div className={"linkArea"}>
-                <h4>{props.name}</h4>
-                <div>
-                    {props.codeUrl ? <a href={props.codeUrl}>源码链接</a> : ''}
-                    {props.previewUrl ?  <a href={props.previewUrl}>项目预览</a> : ''}
+            <div className={"liStyle"}>
+                <div className={"linkArea"}>
+                    <h4>{props.name}</h4>
+                    <div>
+                        {props.codeUrl ? <a href={props.codeUrl}>源码链接</a> : ''}
+                        {props.previewUrl ?  <a href={props.previewUrl}>项目预览</a> : ''}
+                    </div>
                 </div>
+                <ul>
+                    <li>
+                        {props.children}
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li>
-                    {props.children}
-                </li>
-            </ul>
         </div>
     )
 }
